34602ea6fcd7bcf9868cd41bacbca7ecd186855f
mac <mac@macs-iMac.local>
2021-04-08 14:18:40 +0530

    let's learn fast api

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 #instance
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'ravi'}}
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
+    # return published
+    if published:
+        return {'data': f'{limit} published blogs form the databse'}
+    else:
+        return {'data': f'{limit} blogs form the databse'}
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blod'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id,limit=10):
+    return {'data':{'1','2','3'}}
+
+class Blog(BaseModel):
+    title :str
+    body : str
+    published : Optional[bool]
+
+@app.post('/blog')
+def create_blog(request:Blog):
+    return {'data':f"blog is created with title as {request.title}"}
+
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port=9000)
